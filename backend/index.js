92b865239012b736b528a6dd712a8b712bbe9690
Jordan Stone <jstone17@terpmail.umd.edu>
2026-05-16 20:36:56 -0400

    added files

--- backend/index.js
+++ backend/index.js
@@ -6,10 +6,12 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../public'));
 
 const supabaseUrl = 'https://fzthgigzrouzstnyfkyp.supabase.co';
 const supabaseKey = 'sb_publishable_acgI1_XdSDe9PN2Djnq3rg_XRG5uw74';
-const supabase = supabaseClient.createClient(supabaseUrl, supabaseKey);
+const serviceRole = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6dGhnaWd6cm91enN0bnlma3lwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3ODYxMjU4NSwiZXhwIjoyMDk0MTg4NTg1fQ.OrZKadNi6XLIKcDGfM4qmBQoIMJsVQLP2YUW0nh6MQc';
+const supabase = supabaseClient.createClient(supabaseUrl, serviceRole);
 
 app.get('/pets', async (req, res) =>{
   console.log('Attempting to get all data!');
@@ -55,6 +57,32 @@ app.post('/pet', async (req, res) =>{
   } else {
     res.json(data);
   }
+});
+
+app.delete('/pet/:id', async (req, res) => {
+
+  const id = parseInt(req.params.id);
+
+  const { data, error } = await supabase
+    .from('pets')
+    .delete()
+    .eq('id', id)
+    .select();
+
+  if (error) {
+    console.log("DELETE ERROR:");
+    console.log(JSON.stringify(error, null, 2));
+
+    return res.status(500).json(error);
+  }
+
+  console.log("DELETE SUCCESS:");
+  console.log(data);
+
+  res.json({
+    message: "Pet deleted successfully",
+    deleted: data
+  });
 });
 
 app.listen(port, ()=>{
